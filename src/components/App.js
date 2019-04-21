e907374ffd5c398540c637d7f078b51cc418783b
kboul <kos.voulgaridis@gmail.com>
2019-04-21 14:48:03 +0300

    Place Logo & autocomplete columns on UI to be replaced by components & add some margin-top on App

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,17 @@ import Banner from './Banner';
 class App extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="col-md-2">
-                    <Banner />
+            <div className="container-fluid mt-2">
+                <div className="row">
+                    <div className="col-md-2">
+                        <Banner />
+                    </div>
+                    <div className="col-md-2">
+                        Logo
+                    </div>
+                    <div className="col-md-6">
+                        Autocomplete
+                    </div>
                 </div>
             </div>
         )
