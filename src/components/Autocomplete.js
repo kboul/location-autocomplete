2083ef201191c45dc798eb618540ae1ba1278ad0
kboul <kos.voulgaridis@gmail.com>
2019-04-21 21:47:30 +0300

    Listen to onChange event on AsyncTypeahead to store selected value from autocomplete, import ClickToSearchBtn & pass selectedName as prop

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -2,20 +2,31 @@ import React, { Component, Fragment } from 'react'
 import { getLocation } from '../services/locationService'
 import { AsyncTypeahead } from 'react-bootstrap-typeahead'
 import debounce from 'lodash.debounce'
+import ClickToSearchBtn from './ClickToSearchBtn'
 
 class Autocomplete extends Component {
     state = {
+        selectedName: '',
         isLoading: false,
-        entries: []
+        locations: []
     }
 
     handleSearch = debounce(async query => {
         this.setState({ isLoading: true })
-        const { data: { entries } } = await getLocation(query, 'en')
-        this.setState({ isLoading: false, entries })
+        const { data: { entries: locations } } = await getLocation(query, 'en')
+        this.setState({ isLoading: false, locations })
     }, 1000)
 
+    handleChange = selectedOptions => {
+        if (!selectedOptions || !selectedOptions[0]) return
+
+        const selectedName = selectedOptions[0].name
+        this.setState({ selectedName })
+    }
+
     render() {
+        const { locations, selectedName } = this.state
+
         return (
             <Fragment>
                 <div className="mb-2">
@@ -29,11 +40,15 @@ class Autocomplete extends Component {
                     minLength={2}
                     onSearch={this.handleSearch}
                     placeholder="Type a location..."
-                    options={this.state.entries}
+                    options={locations}
+                    onChange={this.handleChange}
                     renderMenuItemChildren={option =>
                         <span>{option.name}</span>
                     }
                 />
+
+                <ClickToSearchBtn
+                    selectedName={selectedName} />
             </Fragment>
         )
     }
