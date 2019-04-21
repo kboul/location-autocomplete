03a23f49be464a14fa3b39b1d983e17a078a98f4
kboul <kos.voulgaridis@gmail.com>
2019-04-21 19:46:27 +0300

    Implement autocomplete functionality

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { getLocation } from '../services/locationService'
+import { AsyncTypeahead } from 'react-bootstrap-typeahead'
 
 class Autocomplete extends Component {
-    state = {}
+    state = {
+        isLoading: false,
+        entries: []
+    }
 
-    async componentDidMount() {
-        const { data: { entries } } = await getLocation('athens', 'en')
-        console.log(entries)
+    handleSearch = async query => {
+        this.setState({ isLoading: true })
+        const { data: { entries } } = await getLocation(query, 'en')
+        this.setState({ isLoading: false, entries })
     }
 
     render() {
         return (
-            <div>What place are you looking for?</div>
+            <Fragment>
+                <div className="mb-2">
+                    What place are you looking for?
+                </div>
+
+                <AsyncTypeahead
+                    id=""
+                    labelKey="name"
+                    {...this.state}
+                    minLength={1}
+                    onSearch={this.handleSearch}
+                    placeholder="Type a location..."
+                    options={this.state.entries}
+                    renderMenuItemChildren={option =>
+                        <span>{option.name}</span>
+                    }
+                />
+            </Fragment>
         )
     }
 }
