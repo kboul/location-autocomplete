29554b9bf89c8735aff4d5e5e97f15eb321c7213
kboul <kos.voulgaridis@gmail.com>
2019-04-22 12:15:43 +0300

    Make getLocation an arrow function

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -7,6 +7,6 @@
 import http from '../services/httpService'
 const apiEndpoint = '/search'
 
-export function getLocation(location, language) {
+export const getLocation = (location, language) => {
     return http.get(`${apiEndpoint}?keywords=${location}&language=${language}`)
 }
