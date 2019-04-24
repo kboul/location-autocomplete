0417f281fc171f9e1d9bee3228ba1420736d4a87
kboul <kos.voulgaridis@gmail.com>
2019-04-24 16:46:52 +0300

    Write tests for handleSearch() on Autocomplete

--- src/components/__tests__/Autocomplete.test.js
+++ src/components/__tests__/Autocomplete.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 import Autocomplete from '../Autocomplete'
 import { AsyncTypeahead } from 'react-bootstrap-typeahead'
 import ClickToSearchBtn from '../ClickToSearchBtn'
@@ -20,9 +22,50 @@ it('contains <ClickToSearchBtn />', () => {
     expect(wrapped.find(ClickToSearchBtn)).toHaveLength(1)
 })
 
+describe('handleSearch()', () => {
+    it('treats isLoading state as expected', () => {
+        wrapped.instance().handleSearch('athens')
+        expect(wrapped.state().isLoading).toBeTruthy()
+    })
+
+    it('updates the state accordingly after fetching locations', () => {
+        wrapped.instance().handleSearch('paris')
+        const mock = new MockAdapter(axios)
+
+        const data = [
+            {
+                country: 'France',
+                language: 'en',
+                name: '18th Arrondissement (Paris), Par'
+            },
+            {
+                country: 'France',
+                language: 'en',
+                name: 'Paris-La Chapelle Station'
+            },
+            {
+                country: 'France',
+                language: 'en',
+                name: '8th Arrondissement (Paris)'
+            }
+        ]
+        const baseUrl = process.env.REACT_APP_API_URL
+        const url = `${baseUrl}?keywords=paris&language=en`
+        mock.onGet(url).reply(200, data)
+
+        axios.get(baseUrl, { params: { keywords: 'paris', language: 'en' } })
+            .then(({ data }) => {
+                expect(wrapped.state().isLoading).toBeFalsy()
+                expect(wrapped.state().locations).toBe(data)
+                expect(wrapped.state().isButtonDisabled).toBeFalsy()
+                wrapped.find(AsyncTypeahead).setState({ options: data })
+                expect(wrapped.find(AsyncTypeahead).prop('options')).toBe(data)
+            })
+    })
+})
+
 it('turns isButtonDisabled to truthy when the input is empty', () => {
     wrapped.instance().handleInputChange('')
-    wrapped.setState({ isButtonDisabled: true })
     expect(wrapped.state().isButtonDisabled).toBeTruthy()
 })
 
