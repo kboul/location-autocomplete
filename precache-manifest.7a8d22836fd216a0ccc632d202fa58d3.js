79696ce0284605fe88496b52bc7d40195584c816
kboul <kos.voulgaridis@gmail.com>
2019-04-24 16:59:46 +0300

    Updates

---
+++ precache-manifest.7a8d22836fd216a0ccc632d202fa58d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e62410f3a3553777fb46",
+    "url": "/location-autocomplete/static/css/main.d50d5670.chunk.css"
+  },
+  {
+    "revision": "e62410f3a3553777fb46",
+    "url": "/location-autocomplete/static/js/main.e9ec4e0c.chunk.js"
+  },
+  {
+    "revision": "968fbda71a8b0c012c03",
+    "url": "/location-autocomplete/static/js/runtime~main.9ae42383.js"
+  },
+  {
+    "revision": "af3106e49bbda5bac8dc",
+    "url": "/location-autocomplete/static/css/2.e462a0b1.chunk.css"
+  },
+  {
+    "revision": "af3106e49bbda5bac8dc",
+    "url": "/location-autocomplete/static/js/2.234858d1.chunk.js"
+  },
+  {
+    "revision": "d9001fbedf64fd637cfc48cb25739f34",
+    "url": "/location-autocomplete/static/media/xe-logo.d9001fbe.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/location-autocomplete/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/location-autocomplete/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/location-autocomplete/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/location-autocomplete/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/location-autocomplete/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2038962c17915cc229def3da42796209",
+    "url": "/location-autocomplete/index.html"
+  }
+];
