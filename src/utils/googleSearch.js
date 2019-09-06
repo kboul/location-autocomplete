a9e07d796c8da11c70b0cf0a504a2f3012aae83b
kboul <kos.voulgaridis@gmail.com>
2019-09-06 12:53:01 +0300

    Add a param & return type definition on googleSearch function

--- src/utils/googleSearch.js
+++ src/utils/googleSearch.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {string} term 
+ * @returns {window global object action} - opens a new browser tab
+ */
+
 export const googleSearch = term => {
     return window.open(`https://www.google.com/search?q=${term}`, '_blank')
 }
